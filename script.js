1bc396593434102c3720aec47da9ebda61c1a0ad
Alejandro <cayetano.verduzco.alejandro@gmail.com>
2024-09-19 01:27:42 -0600

    Alejandro Cayetano Verduzco 18/09/2024

---
+++ script.js
@@ -0,0 +1,40 @@
+alert("Hola desde Script");
+//comenrtario en una sola linea
+/*comentario multi lineas*/
+
+
+//18 09 2024 /cayetano verduzco alejandro
+//comentario en una sola linea
+/*comentario multi lineas*/
+
+//VARIABLES
+/*LAS VARIABLES GUARDAN INFORMACIÓN
+QUE PUEDES REUTILIZAR
+ */
+
+//Declaración de variables 
+
+let nombre= "Alejandro";
+const PI=3.1416;
+var edad=22;
+
+/*Tipos de datos:
+Cadena de taxto (string): "Hola, mundo"
+Número (number): 5,13,14
+Booleanos(boolean);true,false
+*/
+
+//EJEMPLO
+
+let mensaje="Hola mundo";
+let numero=42;
+let Estudiante=true;
+
+//operdores matemáticos
+
+let a=5;
+let b=2;
+let suma=a+b;
+let resta=a-b;
+let multiplicacion=a*b;
+let division=a/b;
